a7e2af92b19b53a25ecb7432ce4c1f9eb3370615
Ahmed Hegazy <ahmedhegazy26105@gmail.com>
2026-04-01 08:04:16 +0200

    Added git cofig file

---
+++ git_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "Ahmed Hegazy"
+git config --global user.email "ahmedhegazy26105@gmail.com"
+git config --global init.defaultBranch main
+
+echo "Git global config updated successfully!"
+git config --global --list
